9f9e2dcabfd933f661b169fb0192988a3017978c
Ib Havn <iha@via.dk>
2021-04-16 15:18:03 +0200

    stdin.h included in display_7seg.h

--- display_7seg.h
+++ display_7seg.h
@@ -24,6 +24,8 @@ Here you you will find the functions you will need to work with the driver.
 #ifndef _DISPLAY_7SEG_H_
 #define _DISPLAY_7SEG_H_
 
+#include <stdint.h>
+
 /* ======================================================================================================================= */
 /**
 \ingroup display_7seg_driver_creation
